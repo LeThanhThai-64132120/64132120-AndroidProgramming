3951dc6b25242f8eed7f29b1ba5d0e599c8be801
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-02-26 17:00:04 +0700

    Chức năng nút Tính Tổng

--- Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Gắn layout tương ứng với file này
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main); // tên của layout liên kết 
+        setContentView(R.layout.activity_main); // tên của layout liên kết
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    // Lắng nghe và xử lý sự kiện lên nút tính Tổng
+    public void XuLyCong(View view){
+
     }
 }
