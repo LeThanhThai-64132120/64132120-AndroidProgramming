67ba55dd919b736267721f3387cf7cab421d78cd
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-04 21:56:51 +0700

    Thực hiện bằng Listener

--- Ex5_AddSubMulDiv_Anynomous/app/src/main/java/com/example/ex5_addsubmuldiv_anynomous/MainActivity.java
+++ Ex5_AddSubMulDiv_Anynomous/app/src/main/java/com/example/ex5_addsubmuldiv_anynomous/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ex5_addsubmuldiv_anynomous;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.strictmode.FragmentStrictMode;
 
 public class MainActivity extends AppCompatActivity {
     EditText editTextSo1;
@@ -22,6 +24,68 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         TimDieuKhien();
+        View.OnClickListener boLangNgheCong = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // xử lý code Cộng ở đây
+                String soThu1 = editTextSo1.getText().toString();
+                String soThu2 = editTextSo2.getText().toString();
+                float soA = Float.parseFloat(soThu1);
+                float soB = Float.parseFloat(soThu2);
+
+                float Cong = soA  + soB;
+                String strKetQua = String.valueOf(Cong);
+                editTextKQ.setText(strKetQua);
+            }
+        };
+         nutCong.setOnClickListener(boLangNgheCong);
+
+
+        // Cách inline anonyous (ẩn danh)
+         nutTru.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 String soThu1 = editTextSo1.getText().toString();
+                 String soThu2 = editTextSo2.getText().toString();
+                 float soA = Float.parseFloat(soThu1);
+                 float soB = Float.parseFloat(soThu2);
+
+                 float Tru = soA  - soB;
+                 String strKetQua = String.valueOf(Tru);
+                 editTextKQ.setText(strKetQua);
+             }
+         });
+         // xử lý Nhân
+         nutNhan.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 String soThu1 = editTextSo1.getText().toString();
+                 String soThu2 = editTextSo2.getText().toString();
+                 float soA = Float.parseFloat(soThu1);
+                 float soB = Float.parseFloat(soThu2);
+
+                 float Nhan = soA  * soB;
+                 String strKetQua = String.valueOf(Nhan);
+                 editTextKQ.setText(strKetQua);
+
+             }
+         });
+
+         // nút Chia
+        nutChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String soThu1 = editTextSo1.getText().toString();
+                String soThu2 = editTextSo2.getText().toString();
+                float soA = Float.parseFloat(soThu1);
+                float soB = Float.parseFloat(soThu2);
+
+                float Chia = soA  / soB;
+                String strKetQua = String.valueOf(Chia);
+                editTextKQ.setText(strKetQua);
+
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
