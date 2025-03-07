737f17f8b8ab7e32c5cb86cee5cc2b6b35fd6dff
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-07 21:44:05 +0700

    Code thực hiện chuyển từ trang Main sang Login

--- Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // tìm điều khiển
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+
+        // SỰ kiện
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lệnh xử lý ở đây
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
         });
     }
 }
