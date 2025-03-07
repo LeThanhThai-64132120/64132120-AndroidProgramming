d81a2b2dfc6b48429c607ddbbb279884efd44c7e
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-07 21:58:25 +0700

    Kiểm tra đăng nhập

--- Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex7_intentlogin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,33 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        Button btnXacNhan = findViewById(R.id.btnOK);
+
+        // thuc hien
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Xử lý đăng nhập
+                // lấy data
+                // B1. Tìm tham chiếu đến điều khiển
+                EditText edtTenDN = (EditText) findViewById(R.id.edtUsername);
+                EditText edtPass = findViewById(R.id.edtPassword);
+                // B2. Lấy data
+                String tenDangNhap = edtTenDN.getText().toString();
+                String matKhau = edtPass.getText().toString();
+
+                // Kiểm tra data
+                if(tenDangNhap.equals("lethanhthai") && matKhau.equals("123")) // Mật khẩu đúng
+                {// Chuyển sang màn hình Home
+
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "THÔNG TIN CỦA BẠN SAI", Toast.LENGTH_LONG);
+                }
+            }
         });
+
     }
 }
