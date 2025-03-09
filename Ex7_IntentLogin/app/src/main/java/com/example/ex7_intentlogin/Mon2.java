02778614d2406f86cf5cb4c0acb550e1b189d206
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-09 17:22:50 +0700

    Tạo giao diện thêm Môn học và Background

--- Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/Mon2.java
+++ Ex7_IntentLogin/app/src/main/java/com/example/ex7_intentlogin/Mon2.java
@@ -3,9 +3,7 @@ package com.example.ex7_intentlogin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,32 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeActivity extends AppCompatActivity {
+public class Mon2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
-
-        // Lấy Intent về
-        Intent intentTuLogin = getIntent();
-        //Lọc ra lấy data chuỗi
-        String tenDN_NhanDuoc = intentTuLogin.getStringExtra("ten_dang_nhap");
-        // Gán vào điều khiển
-        TextView tvTenDN = findViewById(R.id.tvUsername);
-        tvTenDN.setText(tenDN_NhanDuoc);
-
-        ImageView nutVeMain = findViewById(R.id.imgBackMain);
+        setContentView(R.layout.activity_mon2);
+        ImageView nutVeMain = findViewById(R.id.imgBackMain3);
         nutVeMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent veMain = new Intent(HomeActivity.this, MainActivity.class);
+                Intent veMain = new Intent(Mon2.this, HomeActivity.class);
                 startActivity(veMain);
             }
-
-            Button btnMon1 = findViewById(R.id.btnMon1);
-
         });
     }
 }
