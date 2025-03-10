7fd1b0c0331d16d0e9446de7506eca8410697390
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-10 22:22:20 +0700

    Code ListView danh sách Ngôn ngữ lập trình

--- ListView_NgonNguLapTrinh/app/src/main/java/com/example/listview_ngonngulaptrinh/MainActivity.java
+++ ListView_NgonNguLapTrinh/app/src/main/java/com/example/listview_ngonngulaptrinh/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.listview_ngonngulaptrinh;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +16,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+        ListView listViewNNLT;
+        ArrayList<String> dsNNLT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        listViewNNLT = findViewById(R.id.lvNNLT);
+        // B1.Chuẩn bị data, hard-code
+        dsNNLT = new ArrayList<String>();
+        dsNNLT.add("Python");
+        dsNNLT.add("C");
+        dsNNLT.add("C++");
+        dsNNLT.add("C#");
+        dsNNLT.add("Java");
+        // B2.
+        ArrayAdapter<String> adapterNNLT;
+        adapterNNLT = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,dsNNLT);
+
+        // B3.Gắn Adapter
+            listViewNNLT.setAdapter(adapterNNLT);
+        // B4.Gắn bộ lắng nghe và xử lý sự kiện
+        listViewNNLT.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // code here
+                // Chú ý: Biến position: vị trí của Item được Click
+                String giaTriDuocChon = dsNNLT.get(position);
+                // Sử dụng các giá trị này
+                // ta Toast lên
+                Toast.makeText(MainActivity.this, giaTriDuocChon,LENGTH_LONG).show();
+            }
         });
+
+
     }
 }
