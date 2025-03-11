657c83e32c62b146129009dfd8266ae49014c48c
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-11 23:05:38 +0700

    Tạo bảng cửu chương

--- Phep_Cuu_Chuong/app/src/main/java/com/example/phep_cuu_chuong/MainActivity.java
+++ Phep_Cuu_Chuong/app/src/main/java/com/example/phep_cuu_chuong/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.phep_cuu_chuong;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,34 +16,48 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvsoA;
+    TextView tvsoB;
+    TextView tvCheck;
+    EditText edtKQ;
+    Button btnCheck;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        tvsoA = findViewById(R.id.tvsoA);
+        tvsoB = findViewById(R.id.tvsoB);
+        tvCheck = findViewById(R.id.tvCheck);
+        edtKQ = findViewById(R.id.edtKQ);
+        btnCheck = findViewById(R.id.btnTinh);
+        TaoRandomSo();
     }
 
-    public void Tinh(View view){
-        TextView tvsoA = findViewById(R.id.tvsoA);
-        TextView tvsoB = findViewById(R.id.tvsoB);
-        TextView tvCheck = findViewById(R.id.tvCheck);
-        EditText edtKQ = findViewById(R.id.edtKQ);
-
+    void TaoRandomSo (){
         // Tạo số ngẫu nhiên toán hạng
         Random random = new Random();
         int randomSoA = random.nextInt(9) + 1 ; // Từ 1 đến 9
         int randomSoB = random.nextInt(10) + 1 ; // Từ 1 đến 10
 
+        // chuyển về kiểu string
+        String strRandomSoA = String.valueOf(randomSoA);
+        String strRandomSoB = String.valueOf(randomSoB);
+
         // gán giá trị ngẫu nhiên
-        tvsoA.setText(randomSoA);
-        tvsoB.setText(randomSoB);
+        tvsoA.setText(strRandomSoA);
+        tvsoB.setText(strRandomSoB);
 
         String strsoA = tvsoA.getText().toString();
+        String strsoB = tvsoB.getText().toString();
+    }
+    public void Tinh(View view){
+
+
+
+         String strsoA = tvsoA.getText().toString();
         String strsoB = tvsoB.getText().toString();
 
         // Tính toán
@@ -52,6 +67,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Lấy kết quả người dùng nhập vào để kiểm tra
         String strKetQua = edtKQ.getText().toString();
+        int KetQuaUser = Integer.parseInt(strKetQua);
+        if (KetQuaUser == KetQua){
+            tvCheck.setText("Đúng");
+            TaoRandomSo();
+        }
+        else {
+            tvCheck.setText("Sai. Hãy nhập lại");
+        }
 
 
     }
