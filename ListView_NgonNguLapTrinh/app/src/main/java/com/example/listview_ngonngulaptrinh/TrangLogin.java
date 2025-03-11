826aeb98ed9b2fa32d1c6b3c6902168b1a245814
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-11 22:05:35 +0700

    Thêm Login

---
+++ ListView_NgonNguLapTrinh/app/src/main/java/com/example/listview_ngonngulaptrinh/TrangLogin.java
@@ -0,0 +1,30 @@
+package com.example.listview_ngonngulaptrinh;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TrangLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_trang_login);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chuyenNNLT = new Intent(TrangLogin.this, MainActivity.class);
+                startActivity(chuyenNNLT);
+            }
+        });
+    }
+}
