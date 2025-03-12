1b36b97ca17d2b394b196ed485afc01773f5e003
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-12 11:21:58 +0700

    Bảng cửu chương 1

--- Phep_Cuu_Chuong/app/src/main/java/com/example/phep_cuu_chuong/MainActivity.java
+++ Phep_Cuu_Chuong/app/src/main/java/com/example/phep_cuu_chuong/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
     TextView tvsoB;
     TextView tvCheck;
     EditText edtKQ;
-    Button btnCheck;
+    Button btnCheck, btnDelete,  btnSo1, btnSo2, btnSo3, btnSo4, btnSo5, btnSo6, btnSo7, btnSo8, btnSo9, btnSo10;
+
 
 
     @Override
@@ -33,13 +34,24 @@ public class MainActivity extends AppCompatActivity {
         tvCheck = findViewById(R.id.tvCheck);
         edtKQ = findViewById(R.id.edtKQ);
         btnCheck = findViewById(R.id.btnCheck);
+        btnSo1 = findViewById(R.id.btnso1);
+        btnSo3 = findViewById(R.id.btnso3);
+        btnSo4 = findViewById(R.id.btnso4);
+        btnSo5 = findViewById(R.id.btnso5);
+        btnSo6 = findViewById(R.id.btnso6);
+        btnSo7 = findViewById(R.id.btnso7);
+        btnSo8 = findViewById(R.id.btnso8);
+        btnSo9 = findViewById(R.id.btnso9);
+        btnSo10 = findViewById(R.id.btnso10);
+
+
         TaoRandomSo();
     }
 
     void TaoRandomSo (){
         // Tạo số ngẫu nhiên toán hạng
         Random random = new Random();
-        int randomSoA = random.nextInt(9) + 1 ; // Từ 1 đến 9
+        int randomSoA = random.nextInt(1) + 1 ; // Từ 1 đến 9
         int randomSoB = random.nextInt(10) + 1 ; // Từ 1 đến 10
 
         // chuyển về kiểu string
@@ -54,9 +66,6 @@ public class MainActivity extends AppCompatActivity {
         String strsoB = tvsoB.getText().toString();
     }
     public void Tinh(View view){
-
-
-
          String strsoA = tvsoA.getText().toString();
         String strsoB = tvsoB.getText().toString();
 
@@ -69,13 +78,73 @@ public class MainActivity extends AppCompatActivity {
         String strKetQua = edtKQ.getText().toString();
         int KetQuaUser = Integer.parseInt(strKetQua);
         if (KetQuaUser == KetQua){
-            tvCheck.setText("Đúng");
+            tvCheck.setText("Giỏi quá 😍. Đúng rồi nè bạn ơi 👍");
             TaoRandomSo();
         }
         else {
-            tvCheck.setText("Sai. Hãy nhập lại");
+            tvCheck.setText("Sai rồi 😭. Thử lại nhé! ");
         }
+    }
+    public void Delete(View view){
+        edtKQ.setText("0");
+    }
 
+    public void Nhapso1(View v) {
+        String strSo1 = btnSo1.getText().toString();
+        int so1 = Integer.parseInt(strSo1);
+            edtKQ.setText(strSo1);
+    }
 
+        public void Nhapso2(View v){
+            btnSo2 = findViewById(R.id.btnso2);
+            String strSo2 = btnSo2.getText().toString();
+                edtKQ.setText(strSo2);
+        }
+    public void Nhapso3(View v){
+        btnSo3 = findViewById(R.id.btnso3);
+        String strSo3 = btnSo3.getText().toString();
+            edtKQ.setText(strSo3);
+    }
+
+    public void Nhapso4(View v){
+        btnSo4 = findViewById(R.id.btnso4);
+        String strSo4 = btnSo4.getText().toString();
+            edtKQ.setText(strSo4);
+    }
+
+    public void Nhapso5(View v){
+        btnSo5 = findViewById(R.id.btnso5);
+        String strSo5 = btnSo5.getText().toString();
+        edtKQ.setText(strSo5);
+    }
+
+    public void Nhapso6(View v){
+        btnSo6 = findViewById(R.id.btnso6);
+        String strSo6 = btnSo6.getText().toString();
+        edtKQ.setText(strSo6);
+    }
+
+    public void Nhapso7(View v){
+        btnSo7 = findViewById(R.id.btnso7);
+        String strSo7 = btnSo7.getText().toString();
+        edtKQ.setText(strSo7);
+    }
+
+    public void Nhapso8(View v){
+        btnSo8 = findViewById(R.id.btnso8);
+        String strSo8 = btnSo8.getText().toString();
+        edtKQ.setText(strSo8);
+    }
+
+    public void Nhapso9(View v){
+        btnSo9 = findViewById(R.id.btnso9);
+        String strSo9 = btnSo9.getText().toString();
+        edtKQ.setText(strSo9);
+    }
+
+    public void Nhapso10(View v){
+        btnSo10 = findViewById(R.id.btnso10);
+        String strSo10 = btnSo10.getText().toString();
+        edtKQ.setText(strSo10);
     }
 }
