cf5e973a058d17a6641d10892738d8313d289a7e
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-16 18:17:28 +0700

    Modern Material Dashboard

--- Modern_Material_Dashboard/app/src/main/java/com/example/modern_material_dashboard/ClothingActivity.java
+++ Modern_Material_Dashboard/app/src/main/java/com/example/modern_material_dashboard/ClothingActivity.java
@@ -1,6 +1,9 @@
 package com.example.modern_material_dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ClothingActivity extends AppCompatActivity {
 
+    ImageView Back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_clothing);
 
+
+    }
+
+     public void QuayLai(View view){
+        Back = findViewById(R.id.imageViewBack);
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(ClothingActivity.this, MainActivity.class);
+                startActivity(back);
+            }
+        });
     }
 }
