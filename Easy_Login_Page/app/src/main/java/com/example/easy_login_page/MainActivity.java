6944a934fea036803bd206031e67278b904a632a
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-16 17:25:59 +0700

    Trang Đăng nhập đơn giản

--- Easy_Login_Page/app/src/main/java/com/example/easy_login_page/MainActivity.java
+++ Easy_Login_Page/app/src/main/java/com/example/easy_login_page/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.easy_login_page;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username, password;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("ThanhThai") && password.getText().toString().equals("1234"))
+                {
+                    Toast.makeText(MainActivity.this, "Login Successful! 👍", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Login Failed! 😟", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
     }
 }
