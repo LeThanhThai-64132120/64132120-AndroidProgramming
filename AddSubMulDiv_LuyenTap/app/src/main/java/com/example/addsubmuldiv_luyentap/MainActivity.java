ee1b3eee9f472c6dc080728bccaec17042162ece
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-22 21:59:22 +0700

    Luyện tập app +-*/

--- AddSubMulDiv_LuyenTap/app/src/main/java/com/example/addsubmuldiv_luyentap/MainActivity.java
+++ AddSubMulDiv_LuyenTap/app/src/main/java/com/example/addsubmuldiv_luyentap/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.addsubmuldiv_luyentap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         TimDK();
         // Chương trình tính
