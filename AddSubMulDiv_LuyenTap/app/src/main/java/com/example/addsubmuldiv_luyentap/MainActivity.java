a1dcc61a9988d4666cba6b264dbdda281c657816
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-22 20:54:19 +0700

    Chương trình  tính  toán 2 số

--- AddSubMulDiv_LuyenTap/app/src/main/java/com/example/addsubmuldiv_luyentap/MainActivity.java
+++ AddSubMulDiv_LuyenTap/app/src/main/java/com/example/addsubmuldiv_luyentap/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.addsubmuldiv_luyentap;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,93 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.animation.ChildrenAlphaProperty;
+import com.google.android.material.badge.BadgeUtils;
+
 public class MainActivity extends AppCompatActivity {
 
+    EditText editTextSoA, editTextSoB;
+    TextView textViewKQ;
+    Button buttonCong, buttonTru, buttonNhan, buttonChia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        TimDK();
+        // Chương trình tính
+        buttonCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strSoA = editTextSoA.getText().toString();
+                String strSoB = editTextSoB.getText().toString();
+
+                float soA = Float.parseFloat(strSoA);
+                float soB = Float.parseFloat(strSoB);
+                float tong = soA + soB;
+                String strTong = String.valueOf(tong);
+                textViewKQ.setText(strTong);
+                Toast.makeText(MainActivity.this, "Đã chọn Phép Cộng", Toast.LENGTH_SHORT).show();
+            }
         });
+
+        buttonTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strSoA = editTextSoA.getText().toString();
+                String strSoB = editTextSoB.getText().toString();
+
+                float soA = Float.parseFloat(strSoA);
+                float soB = Float.parseFloat(strSoB);
+
+                float hieu = soA - soB;
+                String strHieu = String.valueOf(hieu);
+                textViewKQ.setText(strHieu);
+                Toast.makeText(MainActivity.this, "Đã chọn Phép Trừ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strSoA = editTextSoA.getText().toString();
+                String strSoB = editTextSoB.getText().toString();
+
+                float soA = Float.parseFloat(strSoA);
+                float soB = Float.parseFloat(strSoB);
+
+                float Tich = soA * soB;
+                String strTich = String.valueOf(Tich);
+                textViewKQ.setText(strTich);
+                Toast.makeText(MainActivity.this, "Đã chọn Phép Nhân", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        buttonChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strSoA = editTextSoA.getText().toString();
+                String strSoB = editTextSoB.getText().toString();
+
+                float soA = Float.parseFloat(strSoA);
+                float soB = Float.parseFloat(strSoB);
+
+                float Thuong = soA / soB;
+                String strThuong = String.valueOf(Thuong);
+                textViewKQ.setText(strThuong);
+                Toast.makeText(MainActivity.this, "Đã chọn Phép Chia", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    void TimDK(){
+        editTextSoA= findViewById(R.id.edtSoA);
+        editTextSoB = findViewById(R.id.edtSoB);
+        textViewKQ = findViewById(R.id.tvKetQua);
+        buttonCong = findViewById(R.id.btnCong);
+        buttonTru = findViewById(R.id.btnTru);
+        buttonNhan = findViewById(R.id.btnNhan);
+        buttonChia = findViewById(R.id.btnChia);
     }
 }
