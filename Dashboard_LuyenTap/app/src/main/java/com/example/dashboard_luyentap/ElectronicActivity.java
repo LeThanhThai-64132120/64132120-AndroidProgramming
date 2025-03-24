e6f5077ccb576e4e77bf4a523e3bd097b5b819f7
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-24 10:29:48 +0700

    Luyện tập ListView

--- Dashboard_LuyenTap/app/src/main/java/com/example/dashboard_luyentap/ElectronicActivity.java
+++ Dashboard_LuyenTap/app/src/main/java/com/example/dashboard_luyentap/ElectronicActivity.java
@@ -3,28 +3,28 @@ package com.example.dashboard_luyentap;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ElectronicActivity extends AppCompatActivity {
 
-    CardView electronicCard;
+    ImageView imageViewBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_electronic);
 
-        electronicCard = findViewById(R.id.CardElectronic);
-        electronicCard.setOnClickListener(new View.OnClickListener() {
+        imageViewBack = findViewById(R.id.imgBack);
+        imageViewBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ElectronicActivity.class);
+                Intent intent = new Intent(ElectronicActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
