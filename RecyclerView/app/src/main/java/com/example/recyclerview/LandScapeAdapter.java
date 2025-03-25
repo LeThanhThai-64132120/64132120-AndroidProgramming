d5073e366901e28a3f200a1364a77649b8ff6de2
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-03-25 21:11:57 +0700

    Viết chương trình (Lấy ảnh, caption)

--- RecyclerView/app/src/main/java/com/example/recyclerview/LandScapeAdapter.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/LandScapeAdapter.java
@@ -1,13 +1,58 @@
 package com.example.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class LandScapeAdapter extends RecyclerView.Adapter<   > { // Kế thừa
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemlandHoler> {
+
+    Context context;
+    ArrayList<LandScape> lstData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemlandHoler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem = cai_bom.inflate(R.layout.item_land, parent, false);
+        ItemlandHoler viewholderCreated =  new ItemlandHoler(vItem);
+        return viewholderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemlandHoler holder, int position) {
+
+        // Lấy đối tượng hiển thị
+        LandScape landScapeHienThi = lstData.get(position);
+        //Trích thông tin
+        String caption = landScapeHienThi.getLandCaption();
+        String tenFileAnh  = landScapeHienThi.getLandScapeFileName();
+        // Đặt vào trường thông tin của Holder
+        holder.tvCaption.setText(caption);
+        // Đặt ảnh
+            String  packedName = holder.itemView.getContext().getPackageName();
+            // Lấy id ảnh
+            int imageID = holder.itemView.getResources().getIdentifier(tenFileAnh,"mipmap", packedName);
+        holder.ivLandScape.setImageResource(imageID);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    } // Kế thừa
 
     class ItemlandHoler extends RecyclerView.ViewHolder{
         TextView tvCaption;
@@ -16,7 +61,7 @@ public class LandScapeAdapter extends RecyclerView.Adapter<   > { // Kế thừa
             super(itemView);
             tvCaption = itemView.findViewById(R.id.textViewCaption);
             ivLandScape = itemView.findViewById(R.id.imageViewLand);
-            
+
         }
     }
     
