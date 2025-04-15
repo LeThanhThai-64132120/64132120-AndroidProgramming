2e94e57f6a22c2eb999afb66a34021509aa468d2
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-04-15 16:24:51 +0700

    Fragment dùng Dynamic

--- FragmentExAdynamic/app/src/main/java/com/example/fragmentexadynamic/MainActivity.java
+++ FragmentExAdynamic/app/src/main/java/com/example/fragmentexadynamic/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.fragments;
+package com.example.fragmentexadynamic;
 
 import android.os.Bundle;
 
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.frameContentHolder, new ContentFragment())
+                .add(R.id.frameFooterHolder, new FooterFragment())
+
+                .commit();
     }
 }
