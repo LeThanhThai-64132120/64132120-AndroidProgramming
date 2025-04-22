6887345127d0dd2e9d159d91cd45476ce36461b8
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-04-22 11:48:07 +0700

    Tuỳ chỉnh Main cho Bottom Navigation

--- Navigation/app/src/main/java/vn/edu/tinhoc123/navigation/MainActivity.java
+++ Navigation/app/src/main/java/vn/edu/tinhoc123/navigation/MainActivity.java
@@ -1,24 +1,55 @@
 package vn.edu.tinhoc123.navigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+// tìm
+        bottomNav = findViewById(R.id.bot_nav);
+        // Lắng nghe sk user click vào
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int mnuItemDuocChonID = item.getItemId();
+                if (mnuItemDuocChonID == R.id.mnu_home) {
+                    // thay fragment
+                    Toast.makeText(MainActivity.this, "Thay Home", Toast.LENGTH_SHORT).show();
+                }
+                else if (mnuItemDuocChonID == R.id.mnu_search) {
+                    Toast.makeText(MainActivity.this, "Thay Search", Toast.LENGTH_SHORT).show();
+                }
+                else if (mnuItemDuocChonID == R.id.mnu_frofile) {
+                    Toast.makeText(MainActivity.this, "Thay Profile", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    return false;
+                }
+
+                return true;
+            }
         });
+
+
     }
 }
